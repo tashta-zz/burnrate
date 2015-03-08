6abcaa15b28b565c377413f114313a7850e61676
HarleyKwyn <kwyn.meagher@gmail.com>
2015-03-07 18:33:35 -0800

    Make past and future graphs work and clean up nesting

--- client/graph/graph.js
+++ client/graph/graph.js
@@ -5,47 +5,54 @@ Template.graph.events({
 Template.graph.rendered = function(){
   var self = this;
   // To be replaced by Zach's function
+
   function showTransactions(d){
     // Find the matching data by date and account selection
     // Display in transaction template
-    console.log(d,_.find( tempdata, function(datum){
-      if(d.x == datum._date){
-        return true;
-      }
-    }));
+    console.log(d);
   }
-  Meteor.call('txs',function(err) {
-    if (!err) {
-      var data = getDayRange()
-      data = getGraphTransform(data);
-      console.log(data);
-      var graph_config = {
-        bindto: "#"+self.data.graph_id,
-        data: {
-          //Will be something like tempdata form
-          onmouseover: showTransactions,
-          x: 'date',
-          columns: data['all-accounts'],
-          types:{
-            'credit':'area',
-            'debit':'area',
-            'balance': 'line'
-          },
-          colors:{
-            'credit':'#00FF00',
-            'debit':'#FF0000',
-            'balance': '#0000FF'
-          }
-        },
-        axis:{
-          x:{
-            type:"timeseries"
-          }
-        },
-      };
-      var chart = c3.generate(graph_config);
+
+  Meteor.call('txs',null, null, 'struggling', function(err) {
+    if(err){
+      console.log(err);
+      return;
     }
+    var data;
+    if(self.data.graph_id == "past_graph"){
+      data = getDayRange(null, new Date());
+    }else{
+      data = getDayRange(new Date(), null);
+    }
+
+    data = getGraphTransform(data);
+    console.log(data);
+
+    var graph_config = {
+      bindto: "#"+self.data.graph_id,
+      data: {
+        //Will be something like tempdata form
+        onmouseover: showTransactions,
+        x: 'date',
+        columns: data['all-accounts'],
+        types:{
+          'credit':'area',
+          'debit':'area',
+          'balance': 'line'
+        },
+        colors:{
+          'credit':'#00FF00',
+          'debit':'#FF0000',
+          'balance': '#0000FF'
+        }
+      },
+      axis:{
+        x:{
+          type:"timeseries"
+        }
+      },
+    };
+
+    var chart = c3.generate(graph_config);
+    return;
   });
-
-
 };
