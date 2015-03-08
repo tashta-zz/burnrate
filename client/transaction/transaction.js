4eaf5990aaee4cec6ee8a44a6ded1e6bcbc31466
Tatiana Charkova <tattash@gmail.com>
2015-03-08 12:35:51 -0700

    Fixed transactions table.

--- client/transaction/transaction.js
+++ client/transaction/transaction.js
@@ -1,8 +1,19 @@
 Template.transactions.helpers({
   transactions: function(){
     var transactionDate = Session.get("transactionDate");
-    var activeAccount = Session.get("activeAccount") || 'all-accounts';
     var day = getDay(transactionDate);
-    return day[activeAccount];
+    if ((day !== undefined) && (day.length > 0)){
+      var activeAccount = Session.get("activeAccount") || 'all-accounts';
+      var dayAccount = day[0][activeAccount];
+      return dayAccount.txs || [];
+    }
+  },
+
+  formatTime: function(){
+    return moment(this['transaction-time']).format('MMMM Do YYYY, h:mm:ss a');
+  },
+
+  isPending: function(){
+    return this['is-pending'];
   }
 });
