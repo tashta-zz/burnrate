90572f09c21de21165702af7d4136cdf9c2a1204
Zach Pomerantz <zmp@umich.edu>
2015-03-07 17:18:49 -0800

    Use moment for date ops

--- client/transaction/transaction.js
+++ client/transaction/transaction.js
@@ -1,19 +1,15 @@
 getDayRange = function(startDate, endDate) {
-  var MONTH = 1000 * 60 * 60 * 24 * 30;
-
-  if (!startDate) { startDate = new Date(+(new Date()) - MONTH); }
-  else { startDate = new Date(startDate); } // Date is mutated, so must be copied
-  if (!endDate) { endDate = new Date(+(new Date()) + MONTH); }
-  else { endDate = new Date(endDate); } // Date is mutated, so must be copied
-  // Consider entire days, not timestamps
-  startDate.setHours(0), startDate.setMinutes(0), startDate.setSeconds(0);
-  endDate.setHours(23), endDate.setMinutes(59), endDate.setSeconds(59);
+  if (!startDate) { startDate = moment().subtract(1, 'month'); }
+  if (!endDate) { endDate = moment().add(1, 'month'); }
+  startDate = new Date(moment(startDate).startOf('day'));
+  endDate = new Date(moment(endDate).endOf('day'));
 
   return txs.find({_date: {$gte: startDate, $lte: endDate}})
     .fetch();
 };
 
 getDay = function(date) {
+  if (!date) { date = new Date(); }
   return getDayRange(date, date);
 };
 
